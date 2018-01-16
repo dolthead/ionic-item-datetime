98f019db6d0bdd58addb26f125f7d7f60bccfff8
tdhale <tdhale@xactware.com>
2018-01-15 22:34:09 -0700

    duplicate ion-datetime issue

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    items: any[];
+    startTime: string = "07:00";
 
-  }
+    constructor(public navCtrl: NavController) {
+        this.items = [{title: 'item1'}];
+    }
+
+    addItem() {
+        this.items.push({ title: `item${ (this.items.length + 1) }` });
+    }
 
 }
